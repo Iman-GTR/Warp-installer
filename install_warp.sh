81f7ba94eadd8bb7d97f4c8ea708ae492666aeb1
Iman-GTR <iman8766@yahoo.com>
2024-09-24 19:47:29 +0330

    Update install_warp.sh

--- install_warp.sh
+++ install_warp.sh
@@ -1,9 +1,5 @@
-#!/bin/bash
-
-# تشخیص معماری سیستم
 ARCH=$(uname -m)
 
-# انتخاب فایل مناسب بر اساس معماری
 if [ "$ARCH" = "x86_64" ]; then
     WGCF_FILE="wgcf_2.2.22_linux_amd64"
     WGCF_URL="https://github.com/ViRb3/wgcf/releases/download/v2.2.22/wgcf_2.2.22_linux_amd64"
@@ -15,34 +11,26 @@ else
     exit 1
 fi
 
-# دانلود و نصب wgcf
 wget $WGCF_URL
 mv $WGCF_FILE /usr/bin/wgcf
 chmod +x /usr/bin/wgcf
 
-# ثبت نام و تولید پروفایل
 wgcf register
 wgcf generate
 
-# نصب ابزارهای مورد نیاز
 apt install -y wireguard-tools resolvconf
 
-# اضافه کردن Table = off به فایل پیکربندی
 sed -i '8i Table = off' wgcf-profile.conf
 
-# انتقال فایل پیکربندی
 mv wgcf-profile.conf /etc/wireguard/warp.conf
 
-# فعال کردن و شروع سرویس
 systemctl enable wg-quick@warp
 
-# راه‌اندازی سرویس‌های WireGuard و WARP
 systemctl start wg-quick@warp
 
 echo "WireGuard and WARP services have been successfully started."
 echo "Please note that some changes may require a reboot to fully take effect."
 
-# پرسیدن از کاربر درباره ریبوت
 read -p "Do you want to reboot the system now? (Y/N): " choice
 
 case "$choice" in 
